79e21e64bc2a8aa85b057286025ac865f1059e43
jlucas1224 <82427782+jlucas1224@users.noreply.github.com>
2024-11-04 21:38:18 -0300

    Update Aluno.java

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,14 +1,16 @@
 public class Aluno extends Pessoa{
+    //Da um tab para listar os atributos, o professor vai revisar o código também e não tem como saber se ele vai querer o código limpo ou não, então é melhor deixar ele mais bem formatado possível
+    //Definir a privacidade do atributo também -> private int matricula;
 int matricula;
 
-public Aluno(String nome, String cpf, int idade, int matricula){
-    super(nome, cpf, idade);
-    this.matricula = matricula;
-
-        }
+    public Aluno(String nome, String cpf, int idade, int matricula){
+        super(nome, cpf, idade);
+        this.matricula = matricula;
+    }
+    
+    //Adicionar método pagarMensalidade()
+    //Como não pede um método para exibir dados é melhor não colocar
     public void exibirdados() {
         System.out.println("NOME:" + nome + "\nCPF:" + cpf + "\nIDADE:" + idade + "\nMatrícula:" + matricula + "\n");
     }
-
 }
-
