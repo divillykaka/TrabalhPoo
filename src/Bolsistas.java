da4e813f32e473e3ef8dd7bc312a1065002f51c7
jlucas1224 <82427782+jlucas1224@users.noreply.github.com>
2024-11-04 21:41:26 -0300

    Update Bolsistas.java

--- src/Bolsistas.java
+++ src/Bolsistas.java
@@ -1,8 +1,10 @@
 public class Bolsistas extends Aluno{
-
     public Bolsistas(String nome, String cpf, int idade, int matricula){
         super(nome, cpf, idade, matricula);
     }
+
+    //Criar método pagarMensalidade()
+    //Como não pede um método para exibir dados é melhor não colocar
     public void exibirdados() {
         super.exibirdados();
     }
