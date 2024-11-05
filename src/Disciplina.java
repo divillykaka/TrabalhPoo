febe0f60b5f8b8b7e357404537b0f68ac50e2c9a
jlucas1224 <82427782+jlucas1224@users.noreply.github.com>
2024-11-04 21:42:57 -0300

    Update Disciplina.java

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,8 +1,10 @@
 public class Disciplina {
+    //Mostrar a privacidade do atributo
+    //O atributo se chamar disciplina na classe disciplina pode ficar confuso. O professor pediu os seguintes atributos na classe disciplina: codigo, nome e semestre
     String disciplina;
 
     public Disciplina(String disciplina) {
+        //Adicionar os novos atributos no construtor
         this.disciplina = disciplina;
-
     }
 }
