445762dffe7f46aa44deb67d7ab92897bd428919
jlucas1224 <82427782+jlucas1224@users.noreply.github.com>
2024-11-04 21:47:31 -0300

    Update Professor.java

--- src/Professor.java
+++ src/Professor.java
@@ -1,12 +1,13 @@
 public class Professor extends Pessoa{
-String centro;
+    //Privacidade do atributo
+    String centro;
 
-public Professor(String nome, String cpf, int idade){
-    super(nome, cpf, idade);
-}
-
-void aula() {
-    System.out.println("Dar aula");
-}
-
+    public Professor(String nome, String cpf, int idade){
+        super(nome, cpf, idade);
+        //Inicializar o atributo centro
+    }
+    
+    void darAula() {
+        System.out.println("Dar aula");
+    }
 }
