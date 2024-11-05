5fb31f3a38c63c3aefe079762f822652a3f1abd2
jlucas1224 <82427782+jlucas1224@users.noreply.github.com>
2024-11-04 21:48:23 -0300

    Update Regular.java

--- src/Regular.java
+++ src/Regular.java
@@ -1,8 +1,10 @@
 public class Regular extends Aluno{
-
     public Regular(String nome, String cpf, int idade, int matricula){
         super(nome, cpf, idade, matricula);
     }
+    
+    //Adicionar método pagarMensalidade()
+    //Como não pede um método para exibir dados é melhor não colocar
     public void exibirdados() {
         super.exibirdados();
     }
